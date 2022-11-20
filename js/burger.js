7968385c837b6453daf33b80514e54c94e7bc435
Asaenko1 <117010850+Asaenko1@users.noreply.github.com>
2022-11-20 17:27:58 +0300

    Update burger.js

--- js/burger.js
+++ js/burger.js
@@ -1,24 +1,12 @@
-var burgerTop= document.getElementsByClassName("top");
-var burgerBot= document.getElementsByClassName("bottom");
-var burgerMid= document.getElementsByClassName("middle");
-var burgerSidebar= document.getElementsByClassName("sidebar");
-var sideText= document.getElementsByClassName("sidetext");
-var burgerWhole= document.querySelectorAll(".top, .bottom, .middle");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-var tl = new TimelineMax({paused:true, reversed:true});
-  tl.timeScale(1);
-  tl
-  .to(burgerTop, 0.7, { y: 11, yoyo: true, ease: Power1.easeInOut})
-  .to(burgerBot, 0.7, { y: -11, yoyo: true, ease: Power1.easeInOut}, '-=0.7')
-  .to(burgerTop, 1, {rotation:585})
-  .to(burgerMid, 1, {rotation:585}, '-=1')
-  .to(burgerBot, 1, {rotation:675}, '-=1')
-  .to(burgerWhole, 0.1, {css:{borderColor:"#BE0072"}, ease: Power1.easeOut}, '-=1')
-  .to(burgerSidebar, 1, {x:550, ease: Power2.easeOut}, '-=1')
-  .staggerFrom(sideText, 0.5, {opacity: 0, y: 25, ease: Power1.easeOut}, 0.1, "-=0.7")
-  .to(sideText, 0.2, {css:{color:"#777"}});
-    
-function haminate() {
-  tl.reversed() ? tl.play() : tl.reverse();	
-  
-}
+hamburger.addEventListener("click", () => {
+	hamburger.classList.toggle("active");
+	navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+	hamburger.classList.remove("active");
+	navMenu.classList.remove("active");
+}))
